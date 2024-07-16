7bf386e898013ccc7e74af1e17475c59a80f2b5a
aidqen <idanmarkin8@gmail.com>
2024-07-16 17:41:30 +0300

    Add toy service local to manage demo data in frontend

---
+++ src/services/toy.service.local.js
@@ -0,0 +1,52 @@
+import { storageService } from './async-storage.service'
+import { utilService } from './util.service'
+
+const labels = [
+  'On wheels',
+  'Box game',
+  'Art',
+  'Baby',
+  'Doll',
+  'Puzzle',
+  'Outdoor',
+  'Battery Powered',
+]
+
+const STORAGE_KEY = 'toysDB'
+
+export const toyService = {
+  query,
+}
+
+function query() {
+  storageService.query(STORAGE_KEY).then((toys) => {
+    if (!toys || !toys.length) {
+      const createdToys = createToys()
+      storageService.save(STORAGE_KEY, createdToys)
+      console.log(createToys);
+    }
+  })
+}
+
+export function createToys() {
+  const createdToys = []
+
+  for (let i = 0; i < 15; i++) {
+    const toy = {
+      id: utilService.makeId(),
+      name: utilService.makeLorem(5),
+      price: utilService.getRandomIntInclusive(1, 200),
+      labels: _getRandomLabels(),
+      createdAt: utilService.getRandomTimestamp('01-12-2020', '01-12-2024'),
+      inStock: Math.random() > 0.5,
+    }
+
+    createdToys.push(toy)
+  }
+
+  return createdToys
+}
+
+function _getRandomLabels() {
+  return labels.filter(label => Math.random() > 0.5)
+}
